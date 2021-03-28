313d21b3578d8c2d29d41fbc47cc264fd396f8e6
Jam Adams <wodadehencou@gmail.com>
2021-03-28 08:12:38 +0800

    update

--- conf.d/env.fish
+++ conf.d/env.fish
@@ -1,7 +1,4 @@
-set -gx TODO_DIR $HOME/.local/share/todo.txt
-set -gx TODOTXT_CFG_FILE $TODO_DIR/config
-set -gx TODO_FILE $TODO_DIR/todo.txt
-set -gx DONE_FILE $TODO_DIR/done.txt
+set -gx TODOTXT_CFG_FILE $HOME/.local/share/todo.txt/config
 
 set -Ux LESS "-R"
 set -Ux PAGER less
