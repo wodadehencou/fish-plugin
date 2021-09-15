a4cf65c2da803fe5de5022cefe3e2e7b55615547
jiamu <jiamu@jd.com>
2021-09-15 09:35:42 +0800

    add todo config

--- conf.d/env.fish
+++ conf.d/env.fish
@@ -4,3 +4,7 @@ set -Ux PAGER less
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 
+set -Ux TODO_DIR "$HOME/.local/share/todo"
+set -Ux TODO_FILE "$TODO_DIR/todo.txt"
+set -Ux DONE_FILE "$TODO_DIR/done.txt"
+
