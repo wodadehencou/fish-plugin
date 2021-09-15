ff866ef1f78bdad07aaafa121498a421e6048f5d
jiamu <jiamu@jd.com>
2021-09-15 11:08:15 +0800

    update

--- conf.d/env.fish
+++ conf.d/env.fish
@@ -1,13 +1,9 @@
 
-set -Ux LESS "-R"
-set -Ux PAGER less
-set -Ux EDITOR nvim
-set -Ux VISUAL nvim
+set -gx LESS "-R"
+set -gx PAGER less
+set -gx EDITOR nvim
+set -gx VISUAL nvim
 
-set -Ux TODO_DIR "$HOME/.local/share/todo"
-set -Ux TODO_FILE "$TODO_DIR/todo.txt"
-set -Ux DONE_FILE "$TODO_DIR/done.txt"
-set -Ux REPORT_FILE "$TODO_DIR/report.txt"
-set -Ux TODO_ACTIONS_DIR "$TODO_DIR/actions.d"
+set -Ux TODOTXT_CFG_FILE "$HOME/.local/share/todo/config"
 
 
