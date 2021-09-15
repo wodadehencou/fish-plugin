2804b980d63247d5f4b6278c3ae6d85255955a25
jiamu <jiamu@jd.com>
2021-09-15 09:37:43 +0800

    ADD todo plugins dir

--- conf.d/env.fish
+++ conf.d/env.fish
@@ -7,4 +7,6 @@ set -Ux VISUAL nvim
 set -Ux TODO_DIR "$HOME/.local/share/todo"
 set -Ux TODO_FILE "$TODO_DIR/todo.txt"
 set -Ux DONE_FILE "$TODO_DIR/done.txt"
+set -Ux TODO_ACTIONS_DIR="$TODO_DIR/actions.d"
+
 
