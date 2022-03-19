64a2c3b9d97434237e7aebf8839da9cee1817781
jiamu <jiamu@jd.com>
2022-03-19 14:01:13 +0800

    support cmake --build

--- functions/cmake.fish
+++ functions/cmake.fish
@@ -1,3 +1,10 @@
 function cmake -d "cmake"
-	command cmake $argv -D CMAKE_EXPORT_COMPILE_COMMANDS=ON
+	switch $argv[1]
+		case "--build"
+			command cmake $argv
+		case "--install"
+			command cmake $argv
+		case "*"
+			command cmake $argv -D CMAKE_EXPORT_COMPILE_COMMANDS=ON
+	end
 end
