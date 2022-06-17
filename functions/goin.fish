23b736a82a124d1b3c551f92007a70e4038bcda6
jiamu <jiamu@jd.com>
2022-06-17 17:55:25 +0800

    update

--- functions/goin.fish
+++ functions/goin.fish
@@ -1,4 +1,4 @@
-function jd -d "set jd local proxy"
+function goin -d ""
 	set -gx http_proxy http://127.0.0.1:8118
 	set -gx HTTP_PROXY http://127.0.0.1:8118
 	set -gx https_proxy http://127.0.0.1:8118
