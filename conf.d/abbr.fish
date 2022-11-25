c4b696d9f5c2ebd307116900168c72e0a46e909a
wodadehencou <wodadehencou@gmail.com>
2022-11-25 19:13:55 +0800

    use abbr instead of function

---
+++ conf.d/abbr.fish
@@ -0,0 +1,25 @@
+# git
+abbr -ga 'gaa' 'git add --all'
+abbr -ga 'gau' 'git add --update'
+abbr -ga 'gba' 'git branch --all'
+abbr -ga 'gc' 'git commit'
+abbr -ga 'glog' 'git log --stat --abbrev-commit'
+abbr -ga 'gst' 'git status'
+
+# basic
+abbr -ga 'b' 'cd ..'
+abbr -ga 'q' 'exit'
+abbr -ga 'work' 'cd (HOME)/Workspace'
+
+# editor
+abbr -ga 'gvim' 'vimr'
+abbr -ga 'vim' 'lvim'
+abbr -ga 'vi' 'lvim'
+abbr -ga 'view' 'lvim -R'
+
+# program languages
+abbr -ga 'ldd' 'otool -L'
+
+# translate
+abbr -ga 'en2zh' 'trans -s en -t zh-cn -v'
+abbr -ga 'zh2en' 'trans -s zh-cn -t en -v'
