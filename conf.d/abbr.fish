ad2210cb8a107445ef7d73797fa5a91059b1b75e
wodadehencou <wodadehencou@gmail.com>
2022-11-26 20:35:34 +0800

    update

--- conf.d/abbr.fish
+++ conf.d/abbr.fish
@@ -3,13 +3,14 @@ abbr -ga 'gaa' 'git add --all'
 abbr -ga 'gau' 'git add --update'
 abbr -ga 'gba' 'git branch --all'
 abbr -ga 'gc' 'git commit'
+abbr -ga 'gcm' 'git commit -m'
 abbr -ga 'glog' 'git log --stat --abbrev-commit'
 abbr -ga 'gst' 'git status'
 
 # basic
 abbr -ga 'b' 'cd ..'
 abbr -ga 'q' 'exit'
-abbr -ga 'work' 'cd (HOME)/Workspace'
+abbr -ga 'work' 'cd $HOME/Workspace'
 
 # editor
 abbr -ga 'gvim' 'vimr'
