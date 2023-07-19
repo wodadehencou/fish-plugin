2aeed100a76f35f6152d1018ab8e970e0398ce14
wodadehencou <wodadehencou@gmail.com>
2023-07-19 11:14:26 +0800

    remove podman

--- conf.d/env.fish
+++ conf.d/env.fish
@@ -5,9 +5,9 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 
 # set -Ux TODOTXT_CFG_FILE "$HOME/.local/share/todo/config"
-if test (uname) = Darwin
-	set -gx DOCKER_HOST 'unix:///Users/jiamu/.local/share/containers/podman/machine/podman.sock'
-	set -gx BUILDAH_FORMAT 'docker'
-end
+# if test (uname) = Darwin
+# 	set -gx DOCKER_HOST 'unix:///Users/jiamu/.local/share/containers/podman/machine/podman.sock'
+# 	set -gx BUILDAH_FORMAT 'docker'
+# end
 
 
